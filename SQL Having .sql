ff38b066b7934fb677d5562b97034be8f2dc7781
Dewmi Thinara <dewmithinara68@gmail.com>
2026-01-03 19:08:15 +0530

    Add files via upload

---
+++ SQL Having .sql
@@ -0,0 +1,16 @@
+select * from [dbo].[Sales]
+
+--HAVING--
+
+--For each product, calculate total sales, total quantity, average sale amount, and average quantity, but show only the products where:
+--Total sales < 700 AND
+--Total quantity = 21
+
+select ProductID,
+sum (TotalAmount) [Total Amount],
+sum(Quantity)[Total Quantity],
+avg (TotalAmount) [Average Amount],
+avg (Quantity) [Average Quantity]
+from [dbo].[Sales]
+group by ProductID
+having sum (TotalAmount) < 700 and sum(Quantity) = 21
